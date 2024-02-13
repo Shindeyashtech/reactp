81bf75078317cb5f98fc7a95ddd34d0ac155ad5e
Shindeyashtech <yashshinde.tech@gmail.com>
2024-02-13 10:32:28 +0530

    converter

---
+++ currencyconverter/src/Com/RupeesToDollar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class RupeesToDollar extends Component {
+  constructor(){
+    super()
+    this.state={
+           rs:1
+                
+    }
+    
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
